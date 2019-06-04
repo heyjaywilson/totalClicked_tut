16dc1c10c19593a97eb7a9979c3f8236b3ac6bb5
maeganwilson_ <maegan@maeganwilson.com>
2019-06-04 06:40:45 -0500

    Initial Commit

---
+++ button-push/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  button-push
+//
+//  Created by Maegan Wilson on 6/4/19.
+//  Copyright © 2019 MW. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
